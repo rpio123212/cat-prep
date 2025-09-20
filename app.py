ef38e91516df7bcf50018f4124f23e3f5eefd0a6
rpio123212 <resumeprojects.io@gmail.com>
2025-09-20 08:51:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes, all domains
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+# New GET endpoint
+@app.route('/get', methods=['GET'])
+def get_data():
+    return jsonify({
+        "message": "This is a GET response",
+        "status": "success"
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
